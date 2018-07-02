81577971699972b773a1336fe389452dbd67eadb
Hernán García <hernandario35@hotmail.com>
2018-07-02 18:14:41 -0500

    Estructura Base Ruby on Rails

---
+++ Code/pages/mujer_segura/test/controllers/bienvenido_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class BienvenidoControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get bienvenido_index_url
+    assert_response :success
+  end
+
+end
